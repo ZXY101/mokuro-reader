7c3441c87d07268e3ffba884def64f3a14f902a6
ZXY101 <tennershaun@gmail.com>
2023-08-01 13:33:19 +0200

    Add base styling and Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import { colors } from '$lib/theme';
+
+	type ButtonType = 'primary' | 'secondary' | 'tertiary' | 'danger';
+
+	export let variant: ButtonType;
+
+	function getStyles(buttonType: ButtonType) {
+		switch (buttonType) {
+			case 'primary':
+				return {
+					backgroundColor: colors.primaryColor,
+					color: colors.secondaryColor,
+					activeColor: colors.primaryAccentColor
+				};
+			case 'secondary':
+				return {
+					backgroundColor: colors.secondaryColor,
+					color: colors.primaryColor,
+					activeColor: colors.secondaryAccentColor
+				};
+			case 'tertiary':
+				return {
+					backgroundColor: 'transparent',
+					color: colors.secondaryColor,
+					activeColor: colors.primaryAccentColor
+				};
+			case 'danger':
+				return {
+					backgroundColor: colors.dangerAccentColor,
+					color: colors.dangerColor,
+					activeColor: colors.dangerActivecolor
+				};
+		}
+	}
+
+	const { backgroundColor, color, activeColor } = getStyles(variant);
+</script>
+
+<button
+	style:--color={color}
+	style:--background-color={backgroundColor}
+	style:--active={activeColor}
+	on:click
+>
+	<slot />
+</button>
+
+<style>
+	button {
+		border-style: none;
+		border-radius: 6px;
+		padding: 2px 10px 2px 10px;
+		font-family: var(--font-family);
+		height: 40px;
+		max-height: 40px;
+		font-weight: bold;
+		color: var(--color);
+		border: 1px solid;
+		background-color: var(--background-color);
+	}
+
+	button:hover {
+		cursor: pointer;
+	}
+
+	button:active {
+		background-color: var(--active);
+	}
+</style>
---
+++ src/lib/theme.ts
@@ -0,0 +1,13 @@
+export const colors = {
+	backgroundColor: '#1b1b20',
+
+	primaryColor: '#2e3042',
+	primaryAccentColor: '#263447',
+
+	secondaryColor: '#dfdfe9',
+	secondaryAccentColor: ' #adadbb',
+
+	dangerColor: '#be3329',
+	dangerAccentColor: '#ddaeb2',
+	dangerActivecolor: '#b69092'
+};
