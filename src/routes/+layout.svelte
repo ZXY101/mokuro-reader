1c3af6df13b6d07d0df8d448c466e84837f141a0
ZXY101 <tennershaun@gmail.com>
2023-08-04 15:10:28 +0200

    Add scss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import NavBar from '$lib/components/NavBar.svelte';
 	import Snackbar from '$lib/components/Snackbar.svelte';
-	import '../app.css';
+	import '../app.scss';
 </script>
 
 <NavBar />
