b2810529081fcbc747a53ec60f9ab8fae665ef56
v2lmmj04 <122140891+v2lmmj04@users.noreply.github.com>
2025-01-05 14:51:37 -0800

    Remove underscore prefix for image filenames

--- src/lib/anki-connect/index.ts
+++ src/lib/anki-connect/index.ts
@@ -102,7 +102,7 @@ export async function updateLastCard(imageData: string | null | undefined, sente
         id,
         fields,
         picture: {
-          filename: `_${id}.webp`,
+          filename: `${id}.webp`,
           data: imageData.split(';base64,')[1],
           fields: [pictureField],
         },
